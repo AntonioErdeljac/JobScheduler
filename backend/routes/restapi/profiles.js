7248774556a6c44360890164762fa725a0f4cb30
Antonio Erdeljac <hedhogs@gmail.com>
2017-11-02 20:45:44 +0100

    Implemented post login router

---
+++ backend/routes/restapi/profiles.js
@@ -0,0 +1,7 @@
+var router = require('express').Router();
+
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User');
+
+module.exports = router;
--- backend/routes/restapi/users.js
+++ backend/routes/restapi/users.js
@@ -1,6 +1,8 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var passport = require('passport');
+var auth = require('../auth');
 
 router.post('/users', function(req,res,next){
     console.log('ovdje smo');
@@ -24,6 +26,27 @@ router.post('/users', function(req,res,next){
     return user.save().then(function(){
         return res.json({user: user.toAuthJSON()});
     }).catch(next);
+});
+
+router.post('/users/login', function(req,res,next){
+    if(!req.body.user.email){
+        return res.status(422).json({errors: {email: 'je neispravan'}});
+    }
+
+    if(!req.body.user.password){
+        return res.status(422).json({errors: {password: 'je netočna'}});
+    }
+
+    passport.authenticate('local', {session: false}, function(err, user, info){
+        if(err){return next(err);}
+
+        if(user){
+            user.token = user.generateJWT();
+            return res.json({user: user.toAuthJSON()})
+        } else {
+            return res.status(422).json(info);
+        }
+    })(req,res,next);
 });
 
 router.use(function(err,req,res,next){
