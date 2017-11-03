d1b901dfc12ca6fa9fdf38326eed02fecaa40806
Antonio Erdeljac <hedhogs@gmail.com>
2017-11-03 12:56:52 +0100

    Implemented slack integration on new job

--- backend/routes/restapi/jobs.js
+++ backend/routes/restapi/jobs.js
@@ -6,6 +6,17 @@ var Agenda = require('agenda');
 var slug = require('slug');
 
 
+
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+var bot_token = process.env.SLACK_BOT_TOKEN || 'xoxb-266397551667-uekxKPTL0INks9sWvzEPqOZt';
+
+var rtm = new RtmClient(bot_token);
+
+
+
+
 // agenda se povezuje na DB;
 
 var agenda = new Agenda({db: {address: 'mongodb://localhost/jobscheduler'}});
@@ -20,8 +31,10 @@ agenda.on('ready', function(){
 // definiram osnovni posao
 
 agenda.define('new job', function(job, done){
-    console.log('job started');
-    done();
+    setTimeout(function(){
+        console.log('simulacija nekog procesa');
+        done()
+    }, 500);
 });
 
 
@@ -31,13 +44,50 @@ router.post('/', auth.required, function(req,res,next){
     User.findById(req.payload.id).then(function(user){
         if(!user){return res.sendStatus(401);}
 
+        if(typeof req.body.job.schedule === 'undefined'){
+            return res.status(422).json({errors: {schedule: 'je obavezno'}})
+        }
+
+        if(typeof req.body.job.title === 'undefined'){
+            return res.status(422).json({errors: {title: 'je obavezan'}})
+        }
+
         var scheduleProperty = req.body.job.schedule;
 
         var slugProperty = slug(req.body.job.title) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36);
 
         agenda.schedule(scheduleProperty,'new job', {author: user, uniqueSlug: slugProperty});
 
-        return res.sendStatus(200);
+
+        var channel = 'general';
+
+        rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData){
+            for (const c of rtmStartData.channels) {
+            if (c.is_member && c.name ==='general') { channel = c.id }
+        }
+        console.log('Logged in as '+rtmStartData.self.name+' of team '+rtmStartData.team.name+', but not yet connected to a channel');
+        });
+
+        rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+            rtm.sendMessage("*Novi posao:* "+req.body.job.title+', *Vrjieme*: '+req.body.job.schedule+', *Autor*: '+user.username+' *Slug*: '+slugProperty, channel);
+        });
+
+        rtm.start();
+
+        agenda.jobs({}, function(err, jobs){
+            jobs.map(function(job){
+                if(job.attrs.data.uniqueSlug === slugProperty){
+                    return res.json({
+                        job: job
+                    })
+                }
+            })
+        })
+
+
+
+
+
     })
 });
 
@@ -91,6 +141,9 @@ router.delete('/:slug', auth.required, function(req,res,next){
             })
         })
     }).catch(next);
+});
+
+router.get('/sendmessage', auth.required, function(req,res,next){
 });
 
 
